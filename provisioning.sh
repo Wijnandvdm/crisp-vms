90a99b43c15a13cf297af35a6af0b75c65d420df
Wijnand van der Meijs <wijnandvdmeijs@gmail.com>
2025-10-05 15:17:01 +0200

    updated my provisioning script for more modern way of working

--- provisioning.sh
+++ provisioning.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+UV_VERSION="0.8.0"
 
-sudo apt-get update && sudo apt-get install python3-pip -y # install pip
+sudo apt-get update # update apt packages (one could add sudo apt-get upgrade -y to upgrade all packages, but this take way more time)
 echo "alias python='python3'" >> /home/vagrant/.bashrc # useful alias
 sudo -u vagrant mkdir -p /home/vagrant/repos # creation of repo directory as vagrant for correct permissions
-pip install poetry # install poetry 
-echo 'export PATH="$HOME/.local/bin:$PATH"' >> /home/vagrant/.bashrc # add poetry to PATH
+curl -LsSf https://astral.sh/uv/${UV_VERSION}/install.sh | sh # install uv
 sudo apt-get install docker.io -y # install docker
 sudo usermod -aG docker vagrant # add vagrant user to docker group
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash # install azure cli
+curl -fsSL https://raw.githubusercontent.com/databricks/setup-cli/main/install.sh | sh # install databricks cli
