10cfacda0747fa1db661bcc44f806da9109dad41
Wijnand van der Meijs <wijnandvdmeijs@gmail.com>
2025-10-16 03:33:50 +0000

    removed unnecessary sudo commands since they're executed as root anyway. Left in the sudo command that's used to make a directory as vagrant instead of root since that'd require two commands instead of this single one

--- provisioning.sh
+++ provisioning.sh
@@ -2,14 +2,15 @@
 UV_VERSION="0.8.0"
 DATABRICKS_VERSION=0.268.0
 
-sudo apt-get update # update apt packages (one could add sudo apt-get upgrade -y to upgrade all packages, but this take way more time)
+apt-get update # update apt packages (one could add apt-get upgrade -y to upgrade all packages, but this take way more time)
 echo "alias python='python3'" >> /home/vagrant/.bashrc # useful alias
 sudo -u vagrant mkdir -p /home/vagrant/repos # creation of repo directory as vagrant for correct permissions
 curl -LsSf https://astral.sh/uv/${UV_VERSION}/install.sh | sh # install uv
-sudo apt-get install docker.io -y # install docker
-sudo usermod -aG docker vagrant # add vagrant user to docker group
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash # install azure cli
-sudo apt install unzip # install unzip (necessary for databricks cli installation)
+mv /root/.local/bin/uv /usr/local/bin/ # since uv is installed as root, we need to move it to a system path
+apt-get install docker.io -y # install docker
+usermod -aG docker vagrant # add vagrant user to docker group
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash # install azure cli
+apt install unzip # install unzip (necessary for databricks cli installation)
 curl -fsSL https://raw.githubusercontent.com/databricks/setup-cli/main/install.sh \
   | sed "s/^VERSION=\"[^\"]*\"/VERSION=\"${DATABRICKS_VERSION}\"/" \
-  | sudo sh # install databricks cli
+  | sh # install databricks cli
